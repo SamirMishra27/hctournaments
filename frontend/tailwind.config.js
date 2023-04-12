4bc174d2bebf0d1e8c605c3d301b49f7f26c0e9d
Samir Mishra <samirmishra.a1@gmail.com>
2023-04-12 16:21:04 +0530

    chore(frontend/): setup eslint, prettier & basic styles
    * Setup eslint & prettier configuration in frontend
    * Setup basic styles and import fonts in globals.css
    * Add public/assets folder and a logo to be used later
    * Add some pre-styles in tailwind.config.js
    * Lint whereever necessary

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/app/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+            },
+            backgroundColor: {
+                'red-orange': '#E14D2A',
+                orange: '#FD841F',
+                'soothing-blue': '#3E6D9C',
+                'dark-blue': '#001253'
+            },
+            screens: {
+                xs: '320px',
+                // => @media (min-width: 320px) { ... }
+                tall: { raw: '(min-height: 624px)' }
+                // => @media (min-height: 800px) { ... }
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 }
