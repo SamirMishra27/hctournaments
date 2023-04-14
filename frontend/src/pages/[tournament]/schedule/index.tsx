a4af6309a8e67e0b9f2a3efa47050761d3ba09c1
Samir Mishra <samirmishra.a1@gmail.com>
2023-04-14 23:50:55 +0530

    feat(frontend): add /schedule route in src/pages
    * Add complete page style and route logic for the /schedule page in
      frontend/src/pages/[tournament]/schedule/tsx.
    * Add necessary styles in globals.css

--- frontend/src/pages/[tournament]/schedule/index.tsx
+++ frontend/src/pages/[tournament]/schedule/index.tsx
@@ -1,8 +1,185 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
+import { Fragment } from 'react'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import Link from 'next/link'
 
-export default function schedulePage() {
-    const router = useRouter()
-    const tournamentName = router.query.tournament
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import { axiosApi, getTournamentInfoData } from '@/api'
+import { ApiResponseData, Params } from '@/types'
 
-    return <p>The Schedule page for {tournamentName}!</p>
+export default function schedulePage(props: {
+    tournamentFullName: string
+    season: string
+    serverLink: string
+    embedImageUrl: string
+    schedule: Array<Schedule>
+    results: Array<Schedule>
+}) {
+    function textColor(teamARuns: number, teamBRuns: number) {
+        if (teamARuns > teamBRuns) return 'text-green-100'
+        else if (teamARuns < teamBRuns) return 'text-red-100'
+        else return 'text-slate-200'
+    }
+    // const router = useRouter()
+    // const tournamentName = router.query.tournament
+
+    // return <p>The Schedule page for {tournamentName}!</p>
+    return (
+        <Fragment>
+            <Header />
+            <main className="w-full flex flex-col items-center justify-center page-primary-bg-gradient">
+                <section className="container max-w-[96rem] flex flex-col items-center justify-center py-7 my-5">
+                    <h1 className="text-slate-50 font-bold text-5xl my-3">
+                        {props.tournamentFullName}
+                    </h1>
+                    <h3 className="text-slate-50 uppercase font-semibold text-2xl my-3">
+                        <span className="text-lime-100">{props.season}</span>
+                        <span> - SCHEDULE AND RESULTS</span>
+                    </h3>
+                </section>
+                <hr className="w-[80rem] border-slate-400" />
+
+                <section className="container max-w-[96rem] flex flex-col items-center justify-center py-7 my-5">
+                    <h3 className="text-slate-50 uppercase font-semibold text-3xl my-4">
+                        Upcoming Matches
+                    </h3>
+                    <div
+                        className={
+                            'schedule-list w-[50rem] flex flex-col items-center text-center text-lg ' +
+                            'px-2 py-4 rounded-xl bg-gradient-to-br from-[#19376D] via-[#0B2447] to-[#19376D] ' +
+                            'border-4 border-solid border-[#21315B] text-slate-50 font-semibold ' +
+                            (props.schedule.length
+                                ? 'h-[50rem] overflow-y-scroll '
+                                : 'overflow-y-hidden ')
+                        }>
+                        {props.schedule.length ? (
+                            props.schedule.map((match) => (
+                                <div
+                                    className="w-3/4 row-bg-gradient flex items-center justify-evenly rounded-xl px-3 py-3 my-2"
+                                    key={match.MatchNo}>
+                                    <p>{match.MatchNo}. </p>
+                                    <p className="w-56 text-ellipsis">{match.TeamAName}</p>
+                                    <p className="w-8">VS</p>
+                                    <p className="w-56 text-ellipsis">{match.TeamBName}</p>
+                                </div>
+                            ))
+                        ) : (
+                            <h3 className="text-2xl my-4">
+                                Looks like there are not any upcoming matches
+                            </h3>
+                        )}
+                    </div>
+                </section>
+                <hr className="w-[80rem] border-slate-400" />
+
+                <section className="container max-w-[96rem] flex flex-col items-center justify-center py-7 mt-5 mb-24">
+                    <h3 className="text-slate-50 uppercase font-semibold text-3xl my-4">Results</h3>
+                    <div
+                        className={
+                            'schedule-list w-[50rem] flex flex-col items-center transition-colors text-center text-lg ' +
+                            'px-2 py-4 rounded-xl bg-gradient-to-br from-[#19376D] via-[#0B2447] to-[#19376D] ' +
+                            'border-4 border-solid border-[#21315B] text-slate-50 font-semibold ' +
+                            (props.results.length
+                                ? 'h-[50rem] overflow-y-scroll '
+                                : 'overflow-y-hidden ')
+                        }>
+                        {props.results.length ? (
+                            props.schedule.map((match) => (
+                                <div
+                                    className="w-3/4 row-bg-gradient flex items-center justify-evenly rounded-xl px-3 py-3 my-2"
+                                    key={match.MatchNo}>
+                                    <p>{match.MatchNo}. </p>
+                                    <div>
+                                        <p
+                                            className={
+                                                'w-56 text-ellipsis ' +
+                                                textColor(match.TeamARuns, match.TeamBRuns)
+                                            }>
+                                            {match.TeamAName}
+                                        </p>
+                                        <div className="w-56 text-xs flex items-center justify-evenly">
+                                            <p>
+                                                {match.TeamARuns} / {match.TeamAWickets}
+                                            </p>
+                                            <p>{match.TeamAOvers} OVERS</p>
+                                        </div>
+                                    </div>
+                                    <p className="w-8">VS</p>
+                                    <div>
+                                        <p
+                                            className={
+                                                'w-56 text-ellipsis ' +
+                                                textColor(match.TeamBRuns, match.TeamARuns)
+                                            }>
+                                            {match.TeamBName}
+                                        </p>
+                                        <div className="w-56 text-xs flex items-center justify-evenly">
+                                            <p>
+                                                {match.TeamBRuns} / {match.TeamBWickets}
+                                            </p>
+                                            <p>{match.TeamBOvers} OVERS</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <h3 className="text-2xl my-4">
+                                No matches have finished yet! Check back later
+                            </h3>
+                        )}
+                    </div>
+                </section>
+            </main>
+            <Footer />
+        </Fragment>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const { tournament } = context.params as Params
+
+    const tournamentInfoData = await getTournamentInfoData(tournament)
+    const { season, server_link, tournament_full_name } = tournamentInfoData.data
+
+    const response = await axiosApi.get('/schedule/' + tournament)
+    const scheduleData = response.data as ScheduleApiResponseData
+
+    const embedImageUrl = scheduleData.cloudinary_url
+    const scheduleAndResults = scheduleData.data
+
+    return {
+        props: {
+            tournamentFullName: tournament_full_name,
+            season: season,
+            serverLink: server_link,
+            embedImageUrl: embedImageUrl,
+            schedule: scheduleAndResults.filter((match) => !match.MatchStatus),
+            results: scheduleAndResults.filter((match) => match.MatchStatus)
+        }
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [{ params: { tournament: 'bots' } }, { params: { tournament: 'superleague' } }],
+        fallback: false
+    }
+}
+
+interface Schedule {
+    MatchNo: number
+    MatchStatus: boolean // If true then match is finished
+    TeamAName: string
+    TeamARuns: number
+    TeamAOvers: number
+    TeamAWickets: number
+    TeamBName: string
+    TeamBRuns: number
+    TeamBOvers: number
+    TeamBWickets: number
+}
+
+interface ScheduleApiResponseData extends Omit<ApiResponseData, 'data'> {
+    data: Array<Schedule>
 }
