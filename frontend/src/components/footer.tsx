2245cc80e5a690ca8e22e34c1f737a96113a7811
Samir Mishra <samirmishra.a1@gmail.com>
2023-04-19 20:41:03 +0530

    feat(frontend): responsive design to header and footer components
    Add tailwindcss classes to header and footer components to make them responsive on all screens of width/height. Pages will be made responsive next

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -4,8 +4,8 @@ import { INVITE_BOT_LINK, HCL_SERVER_LINK, BOTS_SERVER_LINK } from '../utils'
 export default function Footer() {
     return (
         <footer className="footer w-full flex items-center justify-center bg-gradient-to-br from-slate-800 to-[#18122B]">
-            <div className="container w-2/4 my-20 flex flex-col items-center justify-evenly text-center">
-                <nav className="w-full flex items-center justify-evenly px-6 text-slate-400 text-lg my-6">
+            <div className="container w-full md:w-4/5 xl:w-2/4 my-10 md:my-20 flex flex-col items-start xs:items-center justify-evenly text-center">
+                <nav className="w-full flex flex-col md:flex-row items-start xs:items-center justify-evenly px-2 xs:px-6 text-slate-400 text-lg my-6 space-y-4 md:space-y-0">
                     <Link
                         href={INVITE_BOT_LINK}
                         className="hover:text-slate-300 active:text-slate-400"
@@ -25,7 +25,7 @@ export default function Footer() {
                         HandCricket Lounge
                     </Link>
                 </nav>
-                <p className="copyright text-slate-200 my-6">© SamirMishra27</p>
+                <p className="copyright text-slate-200 my-6 px-2 xs:px-0">© SamirMishra27</p>
             </div>
         </footer>
     )
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,39 +6,54 @@ import { INVITE_BOT_LINK, HCL_SERVER_LINK, BOTS_SERVER_LINK } from '../utils'
 
 export default function Header() {
     return (
-        <header className="w-full h-20 bg-gradient-to-br from-night-blue-primary to-night-blue-accent flex items-center justify-center">
-            <div className="container max-w-[96rem] h-[95%] flex items-center justify-between px-4">
-                <Link href="/" className="logo h-full px-2 flex items-center justify-center">
-                    <Image src={hclogo} alt="hand cricket" className="logo-img h-3/5 w-auto" />
-                    <span className="logo-text text-3xl font-semibold text-slate-50 [text-shadow:1px_1px_1px_#000000]">
+        <header className="w-full h-36 md:h-20 bg-gradient-to-br from-night-blue-primary to-night-blue-accent flex items-center justify-center">
+            <div className="container max-w-[96rem] h-[95%] flex flex-col md:flex-row items-center justify-evenly md:justify-between px-0 xs:px-4">
+                <Link
+                    href="/"
+                    className="logo w-full xs:w-auto h-full px-2 flex items-center justify-center">
+                    <Image
+                        src={hclogo}
+                        alt="hand cricket"
+                        className="logo-img w-auto h-14 md:h-12"
+                    />
+                    <span className="logo-text text-xl xs:text-4xl md:text-3xl font-semibold text-slate-50 [text-shadow:1px_1px_1px_#000000]">
                         hctournaments
                     </span>
                 </Link>
-                <nav className="nav-links h-full flex items-center justify-evenly px-4 space-x-4">
+                <nav className="w-full xs:w-auto nav-links h-full flex items-center justify-evenly px-4 space-x-4">
                     <button
                         className={
-                            'w-28 h-10 bg-night-blue-primary rounded-xl text-white transition-colors ' +
-                            'hover:bg-[#224587] bg-night-blue-primary'
+                            'w-1/3 xs:w-24 sm:w-32 h-12 bg-night-blue-primary rounded-xl text-white transition-colors ' +
+                            'hover:bg-[#224587] bg-night-blue-primary overflow-clip text-xs xs:text-sm sm:text-base'
                         }>
-                        <Link href={INVITE_BOT_LINK} target="_blank" className="w-full h-full">
+                        <Link
+                            href={INVITE_BOT_LINK}
+                            target="_blank"
+                            className="w-full h-full flex items-center justify-center">
                             Invite Bot
                         </Link>
                     </button>
                     <button
                         className={
-                            'h-10 bg-night-blue-primary rounded-xl text-white transition-colors ' +
-                            'hover:bg-[#224587] bg-night-blue-primary px-3'
+                            'w-1/3 xs:w-24 sm:w-32 h-12 bg-night-blue-primary rounded-xl text-white transition-colors ' +
+                            'hover:bg-[#224587] bg-night-blue-primary overflow-clip'
                         }>
-                        <Link href={HCL_SERVER_LINK} target="_blank" className="w-full h-full">
+                        <Link
+                            href={HCL_SERVER_LINK}
+                            target="_blank"
+                            className="w-full h-full flex items-center justify-center">
                             Super League
                         </Link>
                     </button>
                     <button
                         className={
-                            'w-28 h-10 bg-night-blue-primary rounded-xl text-white transition-colors ' +
-                            'hover:bg-[#224587] bg-night-blue-primary'
+                            'w-1/3 xs:w-24 sm:w-32 h-12 bg-night-blue-primary rounded-xl text-white transition-colors ' +
+                            'hover:bg-[#224587] bg-night-blue-primary overflow-clip'
                         }>
-                        <Link href={BOTS_SERVER_LINK} target="_blank" className="w-full h-full">
+                        <Link
+                            href={BOTS_SERVER_LINK}
+                            target="_blank"
+                            className="w-full h-full flex items-center justify-center">
                             BOTS
                         </Link>
                     </button>
