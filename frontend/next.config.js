ae9ad8268b9ae8e9c687c7c2625ac7354b66f9fd
Samir Mishra <samirmishra.a1@gmail.com>
2023-05-02 23:40:04 +0530

    chore: enable scroll restoration in next.config.js
    In next.config.js, enable scrollRestoration under experimental category,
    so that when navigating back to pages, it persists the exact position
    of page where you left it.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['media.discordapp.net', 'cdn.discordapp.com']
+    },
+    experimental: {
+        scrollRestoration: true
     }
 }
 
