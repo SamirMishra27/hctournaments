2ae86eedf46725b02005f2af732ad01ab5591ba6
Samir Mishra <samirmishra.a1@gmail.com>
2023-09-02 23:52:35 +0530

    feat: add team_standings table model for database
    Add TeamStandings model for team_standings table for database in
    backend.

---
+++ backend/models/team_standings.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, BOOLEAN, CHAR, FLOAT, SMALLINT, VARCHAR
+from .base import BaseModel
+from constants import *
+
+class TeamStandings(BaseModel):
+    __tablename__ = 'team_standings'
+
+    row_id = Column(CHAR(OBJECT_ID_LENGTH), nullable = False, primary_key = True, autoincrement = False)
+    season_no = Column(SMALLINT, nullable = False)
+
+    group_name = Column(VARCHAR(100), nullable = False)
+    group_id = Column(VARCHAR(50), nullable = False)
+
+    team_name = Column(VARCHAR(TEAM_NAME_MAX_LENGTH), nullable = False)
+    matches_played = Column(SMALLINT, nullable = False)
+    matches_won = Column(SMALLINT, nullable = False)
+    matches_lost = Column(SMALLINT, nullable = False)
+
+    points_multiplier = Column(SMALLINT, nullable = False)
+    points = Column(SMALLINT, nullable = False)
+
+    runs_per_wicket_ratio = Column(FLOAT, nullable = True)
+    priority = Column(SMALLINT, nullable = False)
+
+    qualified = Column(BOOLEAN, nullable = True)
