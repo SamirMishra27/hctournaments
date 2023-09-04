de873e1c9448a15a4630dd633efc53d8d883994c
Samir Mishra <samirmishra.a1@gmail.com>
2023-09-04 23:50:00 +0530

    feat: add from_json & to_json methods to tournaments model (utility methods)
    Add utility methods to easily construct a new data object row or to get
    json_data from an existing object row. These are:
    from_json (@classmethod) - call this to construct and return a new data
    object row
    to_json - call this on an existing data object that is tracking the row
    to return it's full json in desired response. (in sqlalchemy)

--- backend/models/tournaments.py
+++ backend/models/tournaments.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, CHAR, SMALLINT, TIMESTAMP, VARCHAR
 from .base import BaseModel
 from constants import *
+from typing import Dict, Union
 
 class Tournaments(BaseModel):
     __tablename__ = 'tournaments'
@@ -26,3 +27,40 @@ class Tournaments(BaseModel):
     embed_theme_link = Column(VARCHAR(250), nullable = True)
 
     champions_team = Column(VARCHAR(TEAM_NAME_MAX_LENGTH), nullable = True)
+
+    @classmethod
+    def from_json(cls, json_data: Dict[str, Union[str, int]]) -> 'Tournaments':
+
+        new_tournament = Tournaments(
+            tournament_id = json_data['tournament_id'],
+            tournament_name = json_data['tournament_name'],
+
+            slug_name = json_data['slug_name'],
+            season_no = json_data['season_no'],
+
+            start_date = json_data['start_date'],
+            end_date = json_data['end_date'],
+            stage = json_data['stage'],
+
+            participants = json_data.get('participants', 0),
+            total_teams = json_data.get('total_teams', 0),
+
+            total_matches = json_data.get('total_matches', 0),
+            matches_done = json_data.get('matches_done', 0),
+
+            server_link = json_data.get('server_link', ''),
+            banner_link = json_data.get('banner_link', ''),
+            embed_theme_link = json_data.get('embed_theme_link', ''),
+
+            champions_team = json_data.get('champions_team', '')
+        )
+        return new_tournament
+
+    def to_json(self) -> Dict[str, Union[str, int]]:
+        json_data = {}
+
+        for key, value in vars(self).items():
+            if not key.startswith('_'):
+                json_data[key] = value
+
+        return json_data
