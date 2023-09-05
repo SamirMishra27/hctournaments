464f91a759a22d3c1aa8f4b80b13028baa549170
Samir Mishra <samirmishra.a1@gmail.com>
2023-09-05 23:40:07 +0530

    feat: implement from_json method for stats & standings models
    Implement from_json model in player stats and team standings model to
    create new row model from json data.

--- backend/models/player_stats.py
+++ backend/models/player_stats.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, CHAR, BIGINT, SMALLINT, VARCHAR
 from .base import BaseModel
 from constants import USERNAME_MAX_LENGTH, OBJECT_ID_LENGTH
+from typing import Dict, Union
 
 class PlayerStats(BaseModel):
     __tablename__ = 'player_stats'
@@ -18,3 +19,19 @@ class PlayerStats(BaseModel):
     balls_given = Column(SMALLINT, nullable = False)
 
     wickets = Column(SMALLINT, nullable = False)
+
+    @classmethod
+    def from_json(self, json_data: Dict[str, Union[str, int]]) -> 'PlayerStats':
+
+        new_player_stats = PlayerStats(
+            row_id = json_data['row_id'],
+            tournament_id = json_data['tournament_id'],
+            user_id = json_data['user_id'],
+            player_name = json_data['player_name'],
+            runs = json_data['runs'],
+            balls = json_data['balls'],
+            runs_given = json_data['runs_given'],
+            balls_given = json_data['balls_given'],
+            wickets = json_data['wickets']
+        )
+        return new_player_stats
--- backend/models/team_standings.py
+++ backend/models/team_standings.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, BOOLEAN, CHAR, FLOAT, SMALLINT, VARCHAR
 from .base import BaseModel
 from constants import *
+from typing import Dict, Union
 
 class TeamStandings(BaseModel):
     __tablename__ = 'team_standings'
@@ -23,3 +24,23 @@ class TeamStandings(BaseModel):
     priority = Column(SMALLINT, nullable = False)
 
     qualified = Column(BOOLEAN, nullable = True)
+
+    @classmethod
+    def from_json(self, json_data: Dict[str, Union[str, int, bool]]) -> 'TeamStandings':
+
+        new_team_standing = TeamStandings(
+            row_id = ['row_id'],
+            tournament_id = ['tournament_id'],
+            group_name = ['group_name'],
+            group_id = ['group_id'],
+            team_name = ['team_name'],
+            matches_played = ['matches_played'],
+            matches_won = ['matches_won'],
+            matches_lost = ['matches_lost'],
+            points_multiplier = ['points_multiplier'],
+            points = ['points'],
+            runs_per_wicket_ratio = json_data.get('runs_per_wicket_ratio', 0),
+            priority = json_data['priority'],
+            qualified = json_data.get('qualified', False)
+        )
+        return new_team_standing
