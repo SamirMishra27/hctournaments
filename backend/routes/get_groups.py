d47c0cf7dab069fb4046af1a2d2b82c5ed20336f
Samir Mishra <samirmishra.a1@gmail.com>
2023-09-07 23:37:31 +0530

    feat: add get groups route & method (any access)
    Add get groups route that will get team standings and groups data of a
    season tournament.

---
+++ backend/routes/get_groups.py
@@ -0,0 +1,65 @@
+from flask import jsonify, make_response, current_app
+from sqlalchemy import and_, select
+
+from custom_types import SessionMaker
+from models import TeamStandings, Tournaments
+from utils import (
+    get_image_from_cloudinary,
+    send_404_json_response,
+    cloud_name
+)
+
+__all__ = ['ROUTE', 'METHOD', '__callback__']
+
+# Define route & method
+ROUTE = '/groups/<string:tournament_slug>/<int:season_no>'
+METHOD = ['GET']
+
+def get_groups(tournament_slug: str, season_no: int):
+
+    # Setup session
+    Session: SessionMaker = current_app.session
+
+    with Session() as session:
+
+        # Get tournament id
+        query = select(Tournaments.tournament_id).where(and_(
+            Tournaments.slug_name == tournament_slug.lower(),
+            Tournaments.season_no == season_no
+        ))
+        tournament_id = session.scalars(query).one_or_none()
+
+        if not tournament_id:
+            return send_404_json_response(
+                success = False,
+                message = 'No tournament with slug `{}` & season `{}` found'.format(tournament_slug, season_no)
+            )
+
+        query = select(TeamStandings).where(TeamStandings.tournament_id == tournament_id)
+        team_standings_data = session.scalars(query).all()
+
+    distinct_group_ids = set([standing.group_id for standing in team_standings_data])
+    image_urls = {}
+
+    for group_id in distinct_group_ids:
+        image_path = f'hctournaments/{tournament_slug}/s{season_no}/groups/{group_id}'
+
+        image, _, _ = get_image_from_cloudinary(
+            public_id = image_path,
+            cloud_name = cloud_name,
+            data_last_edited = None
+        )
+        image_urls[group_id] = '' if image is None else image.get('secure_url')
+
+    team_standings = []
+    for standing_data in team_standings_data:
+        team_standings.append(standing_data.to_json())
+
+    json_data = {
+        'embed_image_url' : image_urls,
+        'team_standings': team_standings
+    }
+    response = make_response(jsonify(json_data), 200)
+    return response
+
+__callback__ = get_groups
