55e85485bc204b45b54d67b5b1961528cdf50db6
Samir Mishra <samirmishra.a1@gmail.com>
2023-09-24 10:00:00 +0530

    chore: update tailwind config to add more theme colours, utilities, font-family, setup dark mode and add daisyui plugin
    Title is self explanatory. Update tailwind.config.js in frontend to add
    more theme colours, more utility styles (skew, fontsize, bozshadow),
    font family, setup dark mode to use class strategy and add daisyui
    plugin. (also set daisyui to use only use light theme).

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-import { screens } from 'tailwindcss/defaultTheme'
+import { screens, fontFamily } from 'tailwindcss/defaultTheme'
 
 module.exports = {
+    daisyui: {
+        themes: ['light']
+    },
+    darkMode: 'class',
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}',
         './src/app/**/*.{js,ts,jsx,tsx}'
     ],
     theme: {
+        fontFamily: {
+            inter: 'Inter, Grotesk Schibsted, Segoe UI, Tahoma, Geneva, Open Sans, Helvetica Neue, Verdana, sans-serif',
+            ...fontFamily
+        },
         screens: {
             xs: '320px',
             // => @media (min-width: 320px) { ... }
@@ -27,9 +35,32 @@ module.exports = {
             colors: {
                 'night-blue-primary': '#19376D',
                 'night-blue-accent': '#0B2447',
-                'soothing-blue': '#3E6D9C'
+                'soothing-blue': '#3E6D9C',
+                // 'orange-gold': '#ffa600',
+                // Old theme pallet
+
+                'dark-blue': '#0b2447',
+                'night-blue': '#19376d',
+                'orange-gold': '#F79327',
+                'full-white': '#ffffff',
+                'dim-white': '#f5f5f5',
+                'dark-navy': '#213555',
+                'bright-navy': '#4F709C'
+            },
+            skew: {
+                8: '8deg'
+            },
+            fontSize: {
+                10: '0.625rem',
+                8: '0.5rem',
+                6: '0.375rem',
+                4: '0.25rem',
+                2: '0.125rem'
+            },
+            boxShadow: {
+                btn: '0 0 0 1px rgba(0,0,0,0.08, transparent)'
             }
         }
     },
-    plugins: []
+    plugins: [require('daisyui')]
 }
