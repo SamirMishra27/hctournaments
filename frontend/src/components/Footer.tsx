b322597214b91243ac36ab892dd0e8103468ecf2
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-05 16:00:00 +0530

    feat: revamp header and footer components & styles
    Change header and footer component styles, add new stuff, improve
    existing ui, add login and dark mode toggle button in header. Shorten
    footer height and make it look more elegant.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,31 +1,45 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { INVITE_BOT_LINK, HCL_SERVER_LINK, BOTS_SERVER_LINK } from '../utils'
+import logoGold from '@assets/trophy-gold.svg'
 
 export default function Footer() {
     return (
-        <footer className="footer w-full flex items-center justify-center bg-gradient-to-br from-slate-800 to-[#18122B]">
-            <div className="container w-full md:w-4/5 xl:w-2/4 my-10 md:my-20 flex flex-col items-start xs:items-center justify-evenly text-left xs:text-center">
-                <nav className="w-full flex flex-col md:flex-row items-start xs:items-center justify-evenly px-2 xs:px-6 text-slate-400 text-lg my-6 space-y-4 md:space-y-0">
+        <footer className=" w-full flex items-center bg-gradient-to-br from-dark-blue to-night-blue px-4 md:px-8 py-10">
+            <div className=" w-full flex flex-col md:flex-row  items-start md:items-center justify-between">
+                <nav className=" flex flex-col md:flex-row items-start md:items-center justify-start [&>a]:transition gap-x-4 md:gap-x-8">
+                    <Link
+                        href="/"
+                        className=" btn no-animation min-h-0 h-8 bg-transparent hover:bg-transparent active:bg-transparent text-sm border-none block hover:no-underline">
+                        <Image
+                            src={logoGold}
+                            alt="cricket trophy logo"
+                            className=" w-full h-full"
+                        />
+                    </Link>
+                    <Link href="/tournaments" className=" text-slate-300 hover:text-slate-100">
+                        Tournaments
+                    </Link>
                     <Link
                         href={INVITE_BOT_LINK}
-                        className="hover:text-slate-300 active:text-slate-400"
+                        className=" text-slate-300 hover:text-slate-100"
                         target="_blank">
                         Invite HandCricket Bot
                     </Link>
                     <Link
                         href={BOTS_SERVER_LINK}
-                        className="hover:text-slate-300 active:text-slate-400"
+                        className=" text-slate-300 hover:text-slate-100"
                         target="_blank">
                         BOTS Server
                     </Link>
                     <Link
                         href={HCL_SERVER_LINK}
-                        className="hover:text-slate-300 active:text-slate-400"
+                        className=" text-slate-300 hover:text-slate-100"
                         target="_blank">
                         HandCricket Lounge
                     </Link>
                 </nav>
-                <p className="copyright text-slate-200 my-6 px-2 xs:px-0">© SamirMishra27</p>
+                <p className="copyright text-slate-200 mt-6 md:m-0">© SamirMishra27</p>
             </div>
         </footer>
     )
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,63 +1,161 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useRouter, usePathname } from 'next/navigation'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import hclogo from '../../public/assets/handcricket.png'
-import { INVITE_BOT_LINK, HCL_SERVER_LINK, BOTS_SERVER_LINK } from '../utils'
+import useDarkMode from '@/hooks/useDarkMode'
+import { getDiscordUser, setCookies } from '@/serverActions'
+import { DiscordUserPayload } from '@/types/payloads'
 
-export default function Header() {
+import { INVITE_BOT_LINK, pathAsId } from '../utils'
+
+import logoGold from '@assets/trophy-gold.svg'
+import moon from '@assets/moon.svg'
+import sun from '@assets/sun.svg'
+import discordLogo from '@assets/discord-logo.svg'
+
+export default function Header(props: { isHomepage?: boolean; pathways: string[][] }) {
+    const { isHomepage = false, pathways } = props
+
+    const [discordUser, setDiscordUser] = useState<DiscordUserPayload | null>(null)
+    const [darkMode, setDarkMode] = useDarkMode()
+
+    const router = useRouter()
+    const pathName = usePathname()
+
+    useEffect(() => {
+        getDiscordUser().then((response) => setDiscordUser(response[1]))
+    }, [])
+
+    function getAvatarUrl(discordUser: DiscordUserPayload) {
+        const { id, avatar } = discordUser
+
+        return `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`
+    }
+
+    function login() {
+        setCookies({ name: 'REDIRECT_AFTER_LOGIN', value: pathName || '/' }).then(() =>
+            router.push('/login')
+        )
+    }
+
     return (
-        <header className="w-full h-36 md:h-20 bg-gradient-to-br from-night-blue-primary to-night-blue-accent flex items-center justify-center">
-            <div className="container max-w-[96rem] h-[95%] flex flex-col md:flex-row items-center justify-evenly md:justify-between px-0 xs:px-4">
-                <Link
-                    href="/"
-                    className="logo w-full xs:w-auto h-full px-2 flex items-center justify-center">
-                    <Image
-                        src={hclogo}
-                        alt="hand cricket"
-                        className="logo-img w-auto h-14 md:h-12"
-                    />
-                    <span className="logo-text text-xl xs:text-4xl md:text-3xl font-semibold text-slate-50 [text-shadow:1px_1px_1px_#000000]">
-                        hctournaments
-                    </span>
-                </Link>
-                <nav className="w-full xs:w-auto nav-links h-full flex items-center justify-evenly px-1 sm:px-4 space-x-1 sm:space-x-4">
-                    <button
+        <header className=" w-screen h-24 md:h-20 bg-night-blue-primary flex items-center justify-evenly">
+            <div className=" max-w-[96rem] w-full flex items-center justify-evenly space-x-2 md:space-x-6 px-2 md:px-6">
+                {isHomepage ? (
+                    <Link
+                        href="/"
+                        className=" w-full xs:w-auto h-full px-2 flex items-center justify-start flex-[3]">
+                        <Image src={logoGold} alt="cricket trophy logo" className=" w-auto h-10" />
+                        <span className=" hidden sm:contents text-xl md:text-2xl font-semibold text-slate-100 [text-shadow:1px_1px_1px_#000000]">
+                            hctournaments
+                        </span>
+                    </Link>
+                ) : (
+                    <nav className=" w-full breadcrumbs flex-[3]">
+                        <ul>
+                            <li>
+                                <Link
+                                    href="/"
+                                    className=" btn no-animation min-h-0 h-10 bg-transparent hover:bg-transparent active:bg-transparent text-sm border-none block hover:no-underline">
+                                    <Image
+                                        src={logoGold}
+                                        alt="cricket trophy logo"
+                                        className=" w-full h-full"
+                                    />
+                                </Link>
+                            </li>
+                            {pathways.map(([pathName, pathLink]) => (
+                                <li key={pathAsId(pathName)} className=" !hidden md:flex">
+                                    <Link
+                                        href={pathLink}
+                                        className={
+                                            ' btn no-animation normal-case min-h-0 h-auto block border-none font-medium py-2 px-4 ' +
+                                            'text-sm bg-transparent hover:bg-stone-50/75 active:bg-stone-50/90 ' +
+                                            'text-dim-white hover:text-black active:text-black hover:no-underline'
+                                        }>
+                                        {pathName}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                )}
+                <nav className=" hidden md:flex items-center justify-evenly gap-x-1 md:gap-x-4 px-1 md:px-4">
+                    <Link
+                        href="/tournaments"
                         className={
-                            'w-1/3 xs:w-24 sm:w-32 h-12 bg-night-blue-primary rounded-xl text-white transition-colors ' +
-                            'hover:bg-[#224587] bg-night-blue-primary overflow-clip text-xs xs:text-sm sm:text-base'
+                            ' btn no-animation normal-case min-h-0 h-auto block border-none font-medium py-2 px-4 ' +
+                            'text-sm bg-transparent hover:underline hover:bg-transparent active:bg-transparent ' +
+                            'hover:no-underline text-dim-white'
                         }>
-                        <Link
-                            href={INVITE_BOT_LINK}
-                            target="_blank"
-                            className="w-full h-full flex items-center justify-center">
-                            Invite Bot
-                        </Link>
-                    </button>
-                    <button
+                        Tournaments
+                    </Link>
+                    <Link
+                        href={INVITE_BOT_LINK}
+                        target="_blank"
                         className={
-                            'w-1/3 xs:w-24 sm:w-32 h-12 bg-night-blue-primary rounded-xl text-white transition-colors ' +
-                            'hover:bg-[#224587] bg-night-blue-primary overflow-clip'
+                            ' btn no-animation normal-case min-h-0 h-auto block border-none font-medium py-2 px-4 ' +
+                            'text-sm bg-transparent hover:underline hover:bg-transparent active:bg-transparent ' +
+                            'hover:no-underline text-dim-white'
                         }>
-                        <Link
-                            href={HCL_SERVER_LINK}
-                            target="_blank"
-                            className="w-full h-full flex items-center justify-center">
-                            Super League
-                        </Link>
-                    </button>
-                    <button
-                        className={
-                            'w-1/3 xs:w-24 sm:w-32 h-12 bg-night-blue-primary rounded-xl text-white transition-colors ' +
-                            'hover:bg-[#224587] bg-night-blue-primary overflow-clip'
-                        }>
-                        <Link
-                            href={BOTS_SERVER_LINK}
-                            target="_blank"
-                            className="w-full h-full flex items-center justify-center">
-                            BOTS
-                        </Link>
-                    </button>
+                        Invite Bot
+                    </Link>
                 </nav>
+                {discordUser ? (
+                    <div className=" flex items-center justify-evenly px-2 gap-x-2">
+                        <h3 className=" text-sm md:text-lg text-dim-white font-semibold">
+                            {discordUser.username}
+                        </h3>
+                        <figure className=" relative">
+                            <Image
+                                src={getAvatarUrl(discordUser)}
+                                alt="Discord user avatar"
+                                className=" rounded-full object-cover w-8 md:w-12 h-8 md:h-12"
+                                quality={100}
+                                width={1024}
+                                height={1024}
+                            />
+                        </figure>
+                    </div>
+                ) : (
+                    <button
+                        className={
+                            ' btn no-animation min-h-0 h-auto bg-transparent hover:bg-transparent' +
+                            ' flex items-center justify-center p-1.5 md:p-2 rounded-md' +
+                            ' bg-night-blue-primary glass'
+                        }
+                        onClick={() => login()}>
+                        <figure className=" relative">
+                            <Image
+                                src={discordLogo}
+                                alt="Discord logo login"
+                                className=" object-cover w-auto h-5 md:h-6"
+                                quality={100}
+                                width={128}
+                                height={97.012}
+                            />
+                        </figure>
+                    </button>
+                )}
+                <button
+                    className={
+                        ' btn no-animation min-h-0 h-auto bg-transparent hover:bg-transparent' +
+                        ' flex items-center justify-center p-1 rounded-md' +
+                        ' bg-night-blue-primary glass'
+                    }
+                    onClick={setDarkMode.toggle}>
+                    <Image
+                        src={darkMode === 'dark' ? moon : sun}
+                        alt={darkMode === 'dark' ? 'Moon' : 'Sun'}
+                        className=" w-6 h-6 md:w-8 md:h-8"
+                        quality={100}
+                        width={512}
+                        height={512}
+                    />
+                </button>
             </div>
         </header>
     )
