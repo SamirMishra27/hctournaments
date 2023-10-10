6c2a39807e478291323c823c4f524367355a97c6
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-10 21:40:22 +0530

    feat: add plain divider component to display a subtle divider line
    Add a plain divider component which will divide components and display a
    subtle line on the screen. (Custom plain divider it is)

---
+++ frontend/src/components/PlainDivider.tsx
@@ -0,0 +1,5 @@
+export default function PlainDivider() {
+    return (
+        <div className=" w-[90%] lg:w-[56rem] xl:w-[64rem] border border-stone-200 h-px my-4 dark:border-stone-200/50 transition" />
+    )
+}
