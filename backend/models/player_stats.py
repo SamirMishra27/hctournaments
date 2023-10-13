61fade8ab636f63a0ecbd1913a5ff4583269413b
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-13 23:58:53 +0530

    feat: add row_no field and convert all char fields as text in player_stats database model
    Add row_no (strictly incrementing unique row number) and name to player_stats
    database model. The row_no is used as sort of an alternative of created
    at timestamp to sort data rows by least recently created.
    
    Convert all data fields in player_stats which use constrained char type, to
    variable character length type or text (TEXT). The reason is that it's
    generally considered a good practise to not enforce limits on a database level.
    Client will handle field limits well.

--- backend/models/player_stats.py
+++ backend/models/player_stats.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, CHAR, BIGINT, SMALLINT, VARCHAR
+from sqlalchemy import Column, CHAR, BIGINT, SMALLINT, TEXT
 from .base import BaseModel
-from constants import USERNAME_MAX_LENGTH, OBJECT_ID_LENGTH
+from constants import OBJECT_ID_LENGTH
 from typing import Dict, Union
 
 class PlayerStats(BaseModel):
@@ -8,9 +8,10 @@ class PlayerStats(BaseModel):
 
     row_id = Column(CHAR(OBJECT_ID_LENGTH), nullable = False, primary_key = True, autoincrement = False, unique = True)
     tournament_id = Column(CHAR(OBJECT_ID_LENGTH), nullable = False)
+    row_no = Column(SMALLINT, nullable = False)
 
     user_id = Column(BIGINT, nullable = False)
-    player_name = Column(VARCHAR(USERNAME_MAX_LENGTH), nullable = False)
+    player_name = Column(TEXT, nullable = False)
 
     runs = Column(SMALLINT, nullable = False)
     balls = Column(SMALLINT, nullable = False)
@@ -26,6 +27,7 @@ class PlayerStats(BaseModel):
         new_player_stats = PlayerStats(
             row_id = json_data['row_id'],
             tournament_id = json_data['tournament_id'],
+            row_no = json_data['row_no'],
             user_id = json_data['user_id'],
             player_name = json_data['player_name'],
             runs = json_data['runs'],
