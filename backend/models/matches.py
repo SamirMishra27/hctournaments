15c4c297d708c3e6fe9b7f7534f7ab91adacb578
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-13 23:56:01 +0530

    feat: add row_no field and convert all char fields as text in matches database model
    Add row_no (strictly incrementing unique row number) and name to matches
    database model. The row_no is used as sort of an alternative of created
    at timestamp to sort data rows by least recently created.
    
    Convert all data fields in matches model which use constrained char type, to
    variable character length type or text (TEXT). The reason is that it's
    generally considered a good practise to not enforce limits on a database level.
    Client will handle field limits well.

--- backend/models/matches.py
+++ backend/models/matches.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, CHAR, FLOAT, SMALLINT, VARCHAR
+from sqlalchemy import Column, CHAR, FLOAT, SMALLINT, TEXT
 from .base import BaseModel
-from constants import *
+from constants import OBJECT_ID_LENGTH
 from typing import Dict, Union
 
 class Matches(BaseModel):
@@ -8,22 +8,23 @@ class Matches(BaseModel):
 
     match_id = Column(CHAR(OBJECT_ID_LENGTH), nullable = False, primary_key = True, autoincrement = False, unique = True)
     tournament_id = Column(CHAR(OBJECT_ID_LENGTH), nullable = False)
+    row_no = Column(SMALLINT, nullable = False)
 
-    title = Column(VARCHAR(250), nullable = False)
-    description = Column(VARCHAR(500), nullable = True)
-    status = Column(VARCHAR(ENUM_MAX_LENGTH), nullable = False)
+    title = Column(TEXT, nullable = False)
+    description = Column(TEXT, nullable = True)
+    status = Column(TEXT, nullable = False)
 
-    team_a_name = Column(VARCHAR(TEAM_NAME_MAX_LENGTH), nullable = False)
+    team_a_name = Column(TEXT, nullable = False)
     team_a_runs = Column(SMALLINT, nullable = True)
     team_a_overs = Column(FLOAT, nullable = True)
     team_a_wickets = Column(SMALLINT, nullable = True)
 
-    team_b_name = Column(VARCHAR(TEAM_NAME_MAX_LENGTH), nullable = False)
+    team_b_name = Column(TEXT, nullable = False)
     team_b_runs = Column(SMALLINT, nullable = True)
     team_b_overs = Column(FLOAT, nullable = True)
     team_b_wickets = Column(SMALLINT, nullable = True)
 
-    winner_name = Column(VARCHAR(TEAM_NAME_MAX_LENGTH), nullable = True)
+    winner_name = Column(TEXT, nullable = True)
 
     @classmethod
     def from_json(cls, json_data: Dict[str, Union[str, int]]) -> 'Matches':
@@ -31,6 +32,7 @@ class Matches(BaseModel):
         new_match = Matches(
             match_id = json_data['match_id'],
             tournament_id = json_data['tournament_id'],
+            row_no = json_data['row_no'],
             title = json_data['title'],
             description = json_data.get('description'),
             status = json_data['status'],
