e52ae8dd84d52bd5e427b28a6958d415b20ca61d
Samir Mishra <samirmishra.a1@gmail.com>
2023-11-25 23:58:37 +0530

    refactor: improve styles in matches page
    Improve styles & proper text alignment in matches page. (For smaller
    screens).

--- frontend/src/app/[tournament]/[seasonNo]/matches/matches.tsx
+++ frontend/src/app/[tournament]/[seasonNo]/matches/matches.tsx
@@ -19,7 +19,7 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                     <article
                         className=" w-full lg:w-[60rem] flex flex-col my-4 rounded-xl border-2 border-dim-white dark:border-bright-navy"
                         key={index}>
-                        <div className=" w-full px-4 py-1 border-b-2 border-b-dim-white rounded-t-xl text-gray-800 text-base md:text-lg font-normal md:font-medium bg-gray-100 dark:bg-bright-navy dark:text-slate-100 transition dark:border-b-bright-navy">
+                        <div className=" w-full px-2 sm:px-4 py-1 border-b-2 border-b-dim-white rounded-t-xl text-gray-800 text-xs xs:text-sm sm:text-base font-normal md:font-medium bg-gray-100 dark:bg-bright-navy dark:text-slate-100 transition dark:border-b-bright-navy">
                             Match {index + 1}
                         </div>
                         <div className=" w-full flex items-center justify-evenly overflow-y-hidden h-24 max-h-24 overflow-x-hidden rounded-b-[10px]">
@@ -27,7 +27,7 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                                 <div
                                     className={
                                         ' w-[110%] md:w-5/6 h-full text-sm xs:text-base sm:text-xl md:text-2xl text-white text-center' +
-                                        ' uppercase font-normal xs:font-medium md:font-bold md:pl-6 -skew-x-8 ' +
+                                        ' uppercase font-normal xs:font-medium sm:font-semibold md:font-bold md:pl-6 -skew-x-8 ' +
                                         ' flex items-center justify-center relative md:-left-6 border-r-2 border-r-dim-white bg-gradient-to-br' +
                                         ' dark:border-r-bright-navy dark:text-slate-100 transition' +
                                         getColour(match.teamAName, match.winnerName, match.status)
@@ -36,7 +36,7 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                                 </div>
                                 {match.status === 'DONE' ? (
                                     <div className="w-full md:w-4/12 flex flex-col items-start justify-center pl-2 md:py-4">
-                                        <div className=" text-sm xs:text-base md:text-xl font-normal md:font-semibold">
+                                        <div className=" text-sm xs:text-base md:text-xl font-normal xs:font-medium md:font-semibold">
                                             {match.teamARuns} / {match.teamAWickets}
                                         </div>
                                         <div className=" uppercase italic text-10 md:text-base">
@@ -46,7 +46,7 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                                 ) : (
                                     <div className="w-full md:w-4/12 flex items-center justify-center py-1 md:py-4">
                                         <p
-                                            className=" text-10 xs:text-base md:text-2xl font-semibold md:mr-5"
+                                            className=" text-10 xs:text-base md:text-2xl font-medium xs:font-semibold md:mr-5"
                                             aria-label="To be decided"
                                             title="To be decided">
                                             TBD
@@ -54,13 +54,17 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                                     </div>
                                 )}
                             </div>
-                            <div className=" w-6 md:w-10 bg-stone-500 text-full-white py-12 text-center -skew-x-8 text-base md:text-xl font-medium dark:text-slate-100 transition z-10">
+                            <div
+                                className={
+                                    ' w-6 md:w-10 bg-stone-500 text-full-white py-12 text-center ' +
+                                    ' -skew-x-8 text-xs sm:text-base md:text-lg font-medium dark:text-slate-100 transition z-10'
+                                }>
                                 VS
                             </div>
                             <div className=" flex flex-1 flex-col-reverse md:flex-row items-center justify-evenly h-full text-gray-800 dark:text-slate-100">
                                 {match.status === 'DONE' ? (
                                     <div className="w-full md:w-4/12 flex flex-col items-end justify-center pr-2 md:py-4">
-                                        <div className=" text-sm xs:text-base md:text-xl font-normal md:font-semibold">
+                                        <div className=" text-sm xs:text-base md:text-xl font-normal xs:font-medium md:font-semibold">
                                             {match.teamBRuns} / {match.teamBWickets}
                                         </div>
                                         <div className=" uppercase italic text-10 md:text-base">
@@ -70,7 +74,7 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                                 ) : (
                                     <div className="w-full md:w-4/12 flex items-center justify-center py-1 md:py-4">
                                         <p
-                                            className=" text-10 xs:text-base md:text-2xl font-semibold md:ml-5"
+                                            className=" text-10 xs:text-base md:text-2xl font-medium xs:font-semibold md:ml-5"
                                             aria-label="To be decided"
                                             title="To be decided">
                                             TBD
@@ -80,7 +84,7 @@ export default function MatchesPage(props: { matches: MatchState[]; imageUrl: st
                                 <div
                                     className={
                                         ' w-[110%] md:w-5/6 h-full text-sm xs:text-base sm:text-xl md:text-2xl text-white text-center' +
-                                        ' uppercase font-normal xs:font-medium md:font-bold md:pr-6 -skew-x-8 ' +
+                                        ' uppercase font-normal xs:font-medium sm:font-semibold md:font-bold md:pr-6 -skew-x-8 ' +
                                         ' flex items-center justify-center relative md:-right-6 border-l border-l-dim-white bg-gradient-to-br' +
                                         ' dark:border-l-bright-navy dark:text-slate-100 transition' +
                                         getColour(match.teamBName, match.winnerName, match.status)
