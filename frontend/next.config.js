767b5caf9dcb20e4284156f5f1c0704c66575f8c
Samir Mishra <samirmishra.a1@gmail.com>
2024-07-16 23:55:55 +0530

    feat: include more domains to show and render images in site on frontend
    Update the config file of next to include and whitelist more domains
    from where images can be seen, including tixtie.net and cloudinary.com
    This is because discord now has limitations on image sharding via cdn.
    So they can no longer be used reliably for image sharing/hosting.
    This will now allow to use tixte and cloudinary hosting services for
    sharing images.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['media.discordapp.net', 'cdn.discordapp.com'],
-        remotePatterns: [{ protocol: 'https', hostname: '*.discordapp.net' }]
+        domains: ['media.discordapp.net', 'cdn.discordapp.com', 'res.cloudinary.com'],
+        remotePatterns: [
+            { protocol: 'https', hostname: '*.discordapp.net' },
+            { protocol: 'https', hostname: '*.cloudinary.com' },
+            { protocol: 'https', hostname: '*.tixte.net' }
+        ]
     },
     experimental: {
         scrollRestoration: true,
